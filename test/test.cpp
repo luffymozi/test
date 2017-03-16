9fe301f5ee13df60164ab2ff935e7019b5b8dd01
luffymozi <1141678635@qq.com>
2017-03-16 23:56:30 +0800

    添加项目文件。

---
+++ test/test.cpp
@@ -0,0 +1,31 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int func();
+void fn();
+static int n = 15;
+int main()
+{
+	func();
+	extern int num;
+	extern  void fn();
+	cout << num << endl;
+	cout << "before" << " " << n << endl;
+	fn();
+	system("pause");
+	return 0;
+}
+int num = 3;
+int func() {
+	cout << num;
+	cout << endl;
+	return 0;
+}
+void fn() {
+
+	n++;
+	cout << "after" << " " << n;
+}
+
